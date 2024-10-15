8038379a0ee3f510a10829142e19b76539e37d6d
daniel <daniel@daniels-MacBook-Pro.local>
2024-10-15 11:28:46 +0200

    update plugins to make mini.pair work with blink.cmp

--- shared/.config/nvim/lua/plugins/lsp-completions.lua
+++ shared/.config/nvim/lua/plugins/lsp-completions.lua
@@ -1,62 +1,64 @@
 return {
-	{
-		"saghen/blink.cmp",
-		lazy = true, -- lazy loading handled internally
-		event = "VeryLazy",
-		-- optional: provides snippets for the snippet source
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		-- use a release tag to download pre-built binaries
-		version = "v0.*",
-		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		opts = {
-			highlight = {
-				-- sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = true,
-			},
-			keymap = {
-				show = "<C-space>",
-				hide = "<C-e>",
-				accept = "<Tab>",
-				select_prev = { "<Up>", "<C-j>" },
-				select_next = { "<Down>", "<C-k>" },
+    {
+        "saghen/blink.cmp",
+        lazy = true, -- lazy loading handled internally
+        event = "VeryLazy",
+        -- optional: provides snippets for the snippet source
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+        },
+        -- use a release tag to download pre-built binaries
+        version = "v0.*",
+        -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        opts = {
+            highlight = {
+                -- sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- useful for when your theme doesn't support blink.cmp
+                -- will be removed in a future release, assuming themes add support
+                use_nvim_cmp_as_default = true,
+            },
+            keymap = {
+                show = "<C-space>",
+                hide = "<C-e>",
+                accept = "<Tab>",
+                select_prev = { "<Up>", "<C-j>" },
+                select_next = { "<Down>", "<C-k>" },
 
-				show_documentation = {},
-				hide_documentation = {},
-				scroll_documentation_up = "<C-b>",
-				scroll_documentation_down = "<C-f>",
+                show_documentation = {},
+                hide_documentation = {},
+                scroll_documentation_up = "<C-b>",
+                scroll_documentation_down = "<C-f>",
 
-				snippet_forward = "<Tab>",
-				snippet_backward = "<S-Tab>",
-			},
-			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "normal",
+                snippet_forward = "<Tab>",
+                snippet_backward = "<S-Tab>",
+            },
+            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- adjusts spacing to ensure icons are aligned
+            nerd_font_variant = "normal",
 
-			-- experimental auto-brackets support
-			accept = { auto_brackets = { enabled = false } },
+            -- experimental auto-brackets support
+            accept = { auto_brackets = { enabled = true } },
 
-			-- experimental signature help support
-			trigger = { signature_help = { enabled = true } },
-			windows = {
-				autocomplete = {
-					border = "rounded",
-					winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
-					draw = "simple",
-				},
-				documentation = {
-					border = "rounded",
-					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
-				},
-				signature_help = {
-					border = "rounded",
-					winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
-				},
-			},
-		},
-	},
+            -- experimental signature help support
+            trigger = { signature_help = { enabled = true } },
+            windows = {
+                autocomplete = {
+                    border = "rounded",
+                    winhighlight =
+                    "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+                    draw = "simple",
+                },
+                documentation = {
+                    border = "rounded",
+                    winhighlight =
+                    "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+                },
+                signature_help = {
+                    border = "rounded",
+                    winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+                },
+            },
+        },
+    },
 }
